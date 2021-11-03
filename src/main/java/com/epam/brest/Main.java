dda4f168966f7c8ef7deb02631a00ac7d5078f39
gerax <sergey.dmitruk.01@mail.ru>
2021-11-03 08:12:48 +0300

    Spring

--- src/main/java/com/epam/brest/Main.java
+++ src/main/java/com/epam/brest/Main.java
@@ -1,9 +1,13 @@
 package com.epam.brest;
 
 import com.epam.brest.files.CSVFileReader;
+import com.epam.brest.files.FileReader;
+import com.epam.brest.model.AbstractStatus;
 import com.epam.brest.model.ReadData;
 import com.epam.brest.model.Status;
 import com.epam.brest.model.StatusType;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -12,9 +16,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        FileReader fileReader = (FileReader) context.getBean("FileReader");
 
-        Map<Integer, BigDecimal> pricePerKgMap = new CSVFileReader().readData("price_weight.csv");
-        Map<Integer, BigDecimal> pricePerKmMap = new CSVFileReader().readData("price_km.csv");
+
+
+
+        Map<Integer, BigDecimal> pricePerKgMap = fileReader.readData("price_weight.csv");
+        Map<Integer, BigDecimal> pricePerKmMap = fileReader.readData("price_km.csv");
+
+
 
         try (Scanner scanner = new Scanner(System.in)) {
             Status readData = new ReadData(scanner, pricePerKgMap, pricePerKmMap);
